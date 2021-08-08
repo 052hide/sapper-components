3847c2e5923cedf19b1688085056e8786abf367c
052hide <hidetoshi1990@gmail.com>
2021-08-08 20:19:00 +0900

    サンプル修正

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,13 @@
   <title>Sapper project template</title>
 </svelte:head>
 
+<div class="p-8">
 <Button
   theme={'secondary'}
   size={'base'}
+  block={true}
 >Button</Button>
+</div>
 
 <script lang='ts'>
   import Button from '../components/Button/index.svelte'
